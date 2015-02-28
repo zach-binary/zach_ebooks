66c609404e2b052cd798f020983c9134ef59d44f
Joey Guerra <joey@joeyguerra.com>
2015-02-28 11:59:42 -0600

    twitter api depends on slf4j
    adding slf4j simple and api to the dependencies to fix that.

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [twitter-api "0.7.8"]]
+                 [twitter-api "0.7.8"]
+                 [org.slf4j/slf4j-api "1.6.2"]
+                 [org.slf4j/slf4j-simple "1.6.2"]]
   :main ^:skip-aot zach-ebooks.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot [zach-ebooks.core]}})
